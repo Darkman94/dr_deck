aafe6af0233626c27c64edfa887dcdca8b59cc4b
George Barton <gbarton@uwo.ca>
2017-08-13 22:22:10 -0400

    added note to secret key, added gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.secret_key = 'jk;adjio;asokj;cai;ohqwdj;xsmlkjquio'
+app.secret_key = 'jk;adjio;asokj;cai;ohqwdj;xsmlkjquio' #change in production
 
 from app import views
